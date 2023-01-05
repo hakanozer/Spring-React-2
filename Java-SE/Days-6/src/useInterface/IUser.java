0e099aa5de4e101d8edf618777138c2d83780789
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-05 21:28:24 +0300

    Revert "Delete"
    This reverts commit 29a5646fe391eca74047c908b6fd08a1ea426034.

---
+++ Java-SE/Days-6/src/useInterface/IUser.java
@@ -0,0 +1,14 @@
+package useInterface;
+
+public interface IUser {
+
+    // interface
+    // içerisinde gövdesiz methodlar kurgulanır
+    // implements anahtar kelimesi ile normal sınıfa özellikleri aktarılmış olurlar.
+    // dokümantasyonu kolay hale getirirler.
+    // interfaceler içindeki methodlara erişim belirteci vermeye gerek yoktur, defaultta public'tir.
+
+    String userName( int uid );
+    int userAge( int uid );
+
+}
