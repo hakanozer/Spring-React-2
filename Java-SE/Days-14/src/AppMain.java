a67e9fc05b9c7523689d58e947006c64d1787490
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-01 15:21:49 +0300

    Days-14

--- Java-SE/Days-14/src/AppMain.java
+++ Java-SE/Days-14/src/AppMain.java
@@ -1,12 +1,58 @@
+import models.User;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class AppMain {
 
     public static void main(String[] args) {
+        UserService service = new UserService();
+        /*
         DB db = new DB();
-        db.connect();
-        db.connect();
-        db.connect();
-        db.connect();
-        db.connect();
+        Connection con1 = db.connect();
+        Connection con2 = db.connect();
+        Connection con3 = db.connect();
+
+        System.out.println( con1.hashCode() );
+        System.out.println( con2.hashCode() );
+        System.out.println( con3.hashCode() );
+         */
+        /*
+        User u = new User("Sultan", "Bilirim", "sultanx@mail.com", "12345", true, 32);
+
+        int statusInsert = service.save(u);
+        if ( statusInsert > 0 ) {
+            System.out.println("Insert Success");
+        }else {
+            System.out.println("Duplicate Email entry Problem");
+        }
+
+        int statusDelete = service.delete(103);
+        System.out.println("statusDelete : " + statusDelete);
+
+
+        //int statusDeleteAll = service.delete(105);
+        //System.out.println("statusDeleteAlls : " + statusDeleteAll);
+
+        User u = new User("Sultan", "Bilirim", "sultany@mail.com", "12345", true, 32);
+        u.setUid(150);
+        int statusUpdate = service.update(u);
+        System.out.println("statusUpdate : " + statusUpdate);
+
+
+        List<User> ls = service.allList();
+        for ( User item : ls ) {
+            System.out.println( item.getUid() );
+        }
+
+        List<User> ls = service.userSearch("a", 20);
+        for ( User item : ls ) {
+            System.out.println( item );
+        }
+         */
+        boolean loginStatus = service.userLogin("zehra@mail.com","6lXYqe");
+        System.out.println(loginStatus);
+
     }
 
 }
---
+++ Java-SE/Days-14/src/UserService.java
@@ -0,0 +1,165 @@
+import models.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class UserService {
+
+    DB db = new DB();
+
+    public int save(User user) {
+        int status = 0;
+        Connection conn = db.connect();
+        try {
+            String sql = "insert into users values( null, ?, ?, ?, ?, ?, ?, now() )";
+            PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setString(1, user.getName());
+            pre.setString(2, user.getSurname());
+            pre.setString(3, user.getEmail());
+            pre.setString(4, user.getPassword());
+            pre.setBoolean(5, user.getStatus());
+            pre.setInt(6, user.getAge());
+            status = pre.executeUpdate();
+        }catch (Exception ex) {
+            System.err.println("Save Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    public int delete(int uid) {
+        int status = 0;
+        Connection conn = db.connect();
+        try {
+            String sql = "delete from users where uid = ?";
+            PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setInt(1, uid);
+            status = pre.executeUpdate();
+        }catch (Exception ex) {
+            System.err.println("Delete Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    public int delete(int... uids) {
+        int status = 0;
+        Connection conn = db.connect();
+        try {
+            for ( int item : uids ) {
+                String sql = "delete from users where uid = ? ";
+                PreparedStatement pre = conn.prepareStatement(sql);
+                pre.setInt(1, item);
+                status = pre.executeUpdate();
+            }
+        }catch (Exception ex) {
+            System.err.println("Delete Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+
+    public int update( User user ) {
+        int status = 0;
+        Connection con = db.connect();
+        try {
+            String sql = "update users set name = ?, surname = ?, email = ?, password = ?, status = ?, age = ? where uid = ?";
+            PreparedStatement pre = con.prepareStatement(sql);
+            pre.setString(1, user.getName());
+            pre.setString(2, user.getSurname());
+            pre.setString(3, user.getEmail());
+            pre.setString(4, user.getPassword());
+            pre.setBoolean(5, user.getStatus());
+            pre.setInt(6, user.getAge());
+            pre.setInt(7, user.getUid());
+            status = pre.executeUpdate();
+        }catch (Exception ex) {
+            System.err.println("Update Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+    public List<User> allList() {
+        List<User> ls = new ArrayList<>();
+        Connection conn = db.connect();
+        try {
+            String sql = "select * from users";
+            PreparedStatement pre = conn.prepareStatement(sql);
+            ResultSet rs = pre.executeQuery();
+            parseTable(ls, rs);
+        }catch (Exception ex) {
+            System.err.println("AllList Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return ls;
+    }
+
+    public List<User> userSearch( String data, int page ) {
+        List<User> ls = new ArrayList<>();
+        Connection conn = db.connect();
+        try {
+            data = "%"+data+"%";
+            String sql = "select * from users where name like ? or surname like ? or email like ? limit ?, 10";
+            PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setString(1, data);
+            pre.setString(2, data);
+            pre.setString(3, data);
+            pre.setInt(4, page);
+            ResultSet rs = pre.executeQuery();
+            parseTable(ls, rs);
+        }catch (Exception ex) {
+            System.err.println("AllList Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return ls;
+    }
+
+    public boolean userLogin( String email, String password ) {
+        boolean status = false;
+        Connection conn = db.connect();
+        try {
+            String sql = "select uid from users where email = ? and password = ?";
+            PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setString(1, email);
+            pre.setString(2, password);
+            ResultSet rs = pre.executeQuery();
+            status = rs.next();
+        }catch (Exception ex) {
+            System.err.println("UserLogin Error : " + ex);
+        }finally {
+            db.close();
+        }
+        return status;
+    }
+
+
+    public void parseTable(List<User> ls, ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            int uid = rs.getInt("uid");
+            String name = rs.getString("name");
+            String surname = rs.getString("surname");
+            String email = rs.getString("email");
+            String password = rs.getString("password");
+            Boolean status = rs.getBoolean("status");
+            Integer age = rs.getInt("age");
+            Date date = rs.getDate("date");
+
+            User u = new User(name,surname, email, password, status, age);
+            u.setUid(uid);
+            u.setDate(date);
+            ls.add(u);
+        }
+    }
+
+
+}
