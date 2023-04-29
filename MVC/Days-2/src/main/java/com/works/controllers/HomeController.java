22d06ccd0d049404d9a1e42ac0f2a9e898061d8e
Hakan ÖZER <hakanozer02@gmail.com>
2023-04-29 10:27:52 +0300

    Delete - BackUp

--- MVC/Days-2/src/main/java/com/works/controllers/HomeController.java
+++ MVC/Days-2/src/main/java/com/works/controllers/HomeController.java
@@ -12,28 +12,38 @@ public class HomeController {
     UserService service = new UserService();
     int status = -1;
     String message = "";
+    int uid = 0;
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("users", service.users());
         model.addAttribute("status", status);
         model.addAttribute("message", message);
+        model.addAttribute("uid", uid);
         status = -1;
         message = "";
+        uid = 0;
         return "home";
     }
 
     @GetMapping("/userDelete/{uid}")
     public String userDelete(@PathVariable int uid) {
-        status = service.deleteUser(uid);
+        status = service.deleteUser(uid, 0);
         if (status > 0) {
             message = "Delete Success - " + uid;
+            this.uid = uid;
         }else {
             message = "Delete Fail - " + uid;
         }
         return "redirect:/";
     }
 
+
+    @GetMapping("/userBack/{uid}")
+    public String userBack(@PathVariable int uid) {
+        service.deleteUser(uid, 1);
+        return "redirect:/";
+    }
 
 
 }
