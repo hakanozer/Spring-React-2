34b483cecd377db011399b872bb3dc4a6564d7dd
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2023-05-11 00:59:46 +0300

    work

--- odevler/JPA/odev_1/JPA-Homework Mert Topal/src/main/java/com/works/jpa/controllers/LoginController.java
+++ odevler/JPA/odev_1/JPA-Homework Mert Topal/src/main/java/com/works/jpa/controllers/LoginController.java
@@ -8,10 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
      final CustomerService customerService;
+     final HttpServletRequest request;
+     final HttpServletResponse response;
      String error = "";
      String success = "";
 
@@ -29,6 +35,10 @@ public class LoginController {
       Customer c = customerService.login(customer.getEmail(),customer.getPassword());
       if(c== null){
           Customer control = customerService.loginCustomer(customer.getEmail());
+          Cookie cookie = new Cookie("customer", ""+c.getCid());
+          cookie.setMaxAge(60 * 60);
+          response.addCookie(cookie);
+
           if(control == null){
               error = "Wrong Mail!";
               return "redirect:/";
@@ -37,9 +47,17 @@ public class LoginController {
               return "redirect:/";
           }
       }
+      request.getSession().setAttribute("customer",c);
       return "redirect:/dashboard";
     }
 
-
+    @GetMapping("/logout")
+    public String logout() {
+        request.getSession().removeAttribute("customer");
+        Cookie cookie = new Cookie("customer", "");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
 
 }
