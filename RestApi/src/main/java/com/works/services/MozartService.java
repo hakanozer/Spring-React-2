21f0a6e46c49fe0bde247c04d968c706aa932797
Hakan ÖZER <hakanozer02@gmail.com>
2023-06-10 11:29:55 +0300

    Update MozartService.java

--- RestApi/src/main/java/com/works/services/MozartService.java
+++ RestApi/src/main/java/com/works/services/MozartService.java
@@ -9,16 +9,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class MozartService {
 
+
     public void result() {
+        String baseUrl = "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/";
         try {
-            String url = "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES.htm";
+            String url = baseUrl+"EARLYSYMPHONIES.htm";
             Document doc = Jsoup.connect(url).timeout(15000).ignoreContentType(true).get();
-            Elements fonts = doc.getElementsByTag("font");
+            Elements fonts = doc.getElementsByAttributeValue("size", "+2");
             for(Element item : fonts) {
-                Elements elements = item.getElementsByAttributeValue("size", "+2");
-                for ( Element itm : elements ) {
-                    System.out.println( itm );
+
+                String objTitle = item.toString();
+                if ( !objTitle.contains("a href=") ) {
+                    String title = item.text().toString();
+                    if ( !title.isEmpty() ) {
+                        System.out.println("Title : " + title);
+                    }
+                }else {
+                    String subTitle = item.text().toString();
+                    if ( !subTitle.isEmpty() ) {
+                        String href = baseUrl + item.getAllElements().attr("href");
+                        System.out.println("-- subTitle : " + subTitle + " url : " + href);
+                    }
                 }
+
+
             }
         }catch (Exception ex) {
             System.err.println("Mozart Error : " + ex);
@@ -26,3 +40,30 @@ public class MozartService {
     }
 
 }
+
+
+/*
+[
+
+{
+    baseTitle: Symphony No.1 in E flat, KV 16
+    musics: [
+       { title: "Allegro molto", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+       { title: "Andante", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+       { title: "Presto", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+    ]
+},
+
+{
+    baseTitle: Symphony No.1 in E flat, KV 16
+    musics: [
+       { title: "Allegro molto", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+       { title: "Andante", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+       { title: "Presto", url: "http://yiboyoung.com/music/mozart/mozart_complete/1EARLIERSYMPHONIES/EARLYSYMPHONIES_files/01a01.mp3" },
+    ]
+}
+
+]
+
+
+ */
